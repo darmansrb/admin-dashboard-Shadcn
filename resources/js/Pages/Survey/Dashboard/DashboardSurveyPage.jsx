91b4215e98108fd747f0f0a3d0b8dc31c5369305
darmansrb <barnabassarbunan@gmail.com>
2025-04-21 08:20:41 +0700

    ubah penamaan folder Components

--- resources/js/Pages/Survey/Dashboard/DashboardSurveyPage.jsx
+++ resources/js/Pages/Survey/Dashboard/DashboardSurveyPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { AdminLayout } from "@/Layouts/AdminLayout";
 import { StatCard } from "@/Components/ui/StatCard";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
 
 const DashboardSurveyPage = () => {
   return (
